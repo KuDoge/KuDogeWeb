fa6f2ca369525a900c235016932a31e8f8049cc5
PrettyShill <>
2021-07-09 07:21:31 +0900

    Switched to external data provider

--- js/updatevalues.js
+++ js/updatevalues.js
@@ -1,13 +1,15 @@
 
 jQuery(document).ready(function($){
-  $.get("/stats/", function(data, status){
-    if(status == "success") {
-      console.log(data);
-      $("#kudo-stat-price").html(data.price);
-      $("#kudo-stat-mcap").html(data.marketcap);
-      $("#kudo-stat-holders").html(data.holder);
-      $("#kudo-stat-supply").html("<span>Current Supply:</span>"+data.supply);
-      $(".token-stats-container").removeClass("loading-data");
-    }
+  $.ajax({
+      url: 'https://token-stats.herokuapp.com/?token=KuDoge&jsonpCallback=jsonCallback',
+      dataType: 'jsonp',
+      jsonpCallback: "jsonCallback",
+      success: function(data){
+        $("#kudo-stat-price").html(data.price);
+        $("#kudo-stat-mcap").html(data.marketcap);
+        $("#kudo-stat-holders").html(data.holder);
+        $("#kudo-stat-supply").html("<span>Current Supply:</span>"+data.supply);
+        $(".token-stats-container").removeClass("loading-data");
+      }
   });
 });
